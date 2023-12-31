cfdaaa45bc17434e8e8f3f08309cd46433dec4a6
Steve <stephen.ancliffe@gmail.com>
2023-12-31 11:14:48 +0700

    Update Google.py
    Added better exception and error handling.

--- Google.py
+++ Google.py
@@ -8,25 +8,33 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your/credentials.json"
 # Instantiate the translation client
 client = translate.Client()
 
-# Function to translate a subtitle line
+# Function to translate a subtitle line, handling potential errors
 def translate_line(line, target_language):
-    translation = client.translate(line.text, target_language=target_language)
-    return pysubs2.SSALine(start=line.start, end=line.end, text=translation['translatedText'])
+    try:
+        translation = client.translate(line.text, target_language=target_language)
+        return pysubs2.SSALine(start=line.start, end=line.end, text=translation['translatedText'])
+    except Exception as e:
+        print(f"Error translating line: {e}")
+        return line  # Return original line if translation fails
 
-# Function to translate an SRT file
+# Function to translate an SRT file, incorporating error handling and progress feedback
 def translate_srt(srt_file, target_language):
-    subs = pysubs2.load(srt_file, encoding="utf-8")
-    translated_subs = subs.copy()
+    try:
+        subs = pysubs2.load(srt_file, encoding="utf-8")
+        translated_subs = subs.copy()
 
-    for line in translated_subs:
-        translated_line = translate_line(line, target_language)
-        line.text = translated_line.text
+        for i, line in enumerate(translated_subs):
+            translated_line = translate_line(line, target_language)
+            translated_subs.lines[i] = translated_line
+            print(f"Translated line {i+1}/{len(translated_subs)}")  # Show progress
 
-    # Save the translated SRT file
-    translated_srt_file = os.path.splitext(srt_file)[0] + "_translated.srt"
-    translated_subs.save(translated_srt_file)
+        # Save the translated SRT file
+        translated_srt_file = os.path.splitext(srt_file)[0] + "_translated.srt"
+        translated_subs.save(translated_srt_file)
 
-    print(f"Subtitle file translated and saved as: {translated_srt_file}")
+        print(f"Subtitle file translated and saved as: {translated_srt_file}")
+    except Exception as e:
+        print(f"Error processing SRT file: {e}")
 
 # Example usage
 srt_file = "path/to/your/subtitle.srt"
